92049e46451666938768ba31644110286d7c0309
atoool <scrapycoco.sam@gmail.com>
2021-11-16 23:53:49 +0530

    vendors add,edit n delete

--- src/views/warehouse/Vendors.js
+++ src/views/warehouse/Vendors.js
@@ -19,7 +19,6 @@ import { Delete, Edit } from "@mui/icons-material";
 import { Modal } from "component/Modal/Modal";
 import { get, post } from "api/api";
 import { AppContext } from "context/AppContext";
-import capitalizeFirstLetter from "utils/capitalizeFirstLetter";
 import { Loader } from "component/loader/Loader";
 import { useSnackbar } from "notistack";
 
@@ -31,6 +30,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontWeight: "bold",
     height: 60,
     padding: 2,
+    textAlign: "left",
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 12,
@@ -49,56 +49,259 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-const data = {
-  name: "",
-  address: "",
-  phone: "",
-  email: "",
-  gstno: "",
-  pancard: "",
-};
-const values = ["Name", "Address", "Phone", "Email", "GST", "PanCard"];
-
+const keys = [
+  {
+    name: "",
+    address: "",
+    phone: "",
+    email: "",
+    gstno: "",
+    pancard: "",
+  },
+];
+const head = ["Name", "Address", "Phone", "Email", "GST", "PanCard"];
 export function Vendors() {
-  const { userData } = React.useContext(AppContext);
+  const { userData, setProductData } = React.useContext(AppContext);
   const token = userData?.token?.accessToken ?? "";
-  let [vendor, setVendor] = React.useState(data);
 
-  const onAddVendor = async () => {
+  const [page, setPage] = React.useState("product");
+  const [open, setOpen] = React.useState(false);
+  let [products, setProducts] = React.useState(keys);
+  let [data, setData] = React.useState(keys);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [currentPage, setCurrentPage] = React.useState(0);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const onClear = () => {
+    setProducts([
+      {
+        name: "",
+        address: "",
+        phone: "",
+        email: "",
+        gstno: "",
+        pancard: "",
+      },
+    ]);
+  };
+
+  const handleClickOpenModal = (pg = "product", i = 0) => {
+    if (pg === "product") {
+      const {
+        id = "",
+        name = "",
+        address = "",
+        phone = "",
+        email = "",
+        gstno = "",
+        pancard = "",
+      } = data[i];
+      setProducts([{ id, name, address, phone, email, gstno, pancard }]);
+    }
+    setPage(pg);
+    setOpen(true);
+  };
+  const onProductFetch = async () => {
     try {
-      await post("new-vendor", token, vendor);
+      const data1 = (await get("list-vendors", token)) ?? [];
+      data1?.data?.response && setData(data1?.data?.response);
+      data1?.data?.response && setProductData(data1?.data?.response);
     } catch {}
   };
 
-  const handleChange = (e, itm) => {
-    const temp = { ...vendor };
-    temp[itm] = e.target.value;
-    setVendor(temp);
+  React.useEffect(() => {
+    onProductFetch();
+  }, []);
+
+  const onAddProducts = async () => {
+    try {
+      await post("new-vendor", token, products);
+      await onProductFetch();
+    } catch {}
   };
-  return (
-    <Loader>
-      {Object.keys(data)?.map((f, i) => (
-        <TextField
-          key={i}
-          required
-          label={values[i]}
-          size="small"
-          sx={{ m: 2 }}
-          value={vendor[f] ?? ""}
-          onChange={(txt) => handleChange(txt, f)}
-        />
-      ))}
+
+  const onEditProduct = async () => {
+    try {
+      const dat = products[0];
+      await post("edit-vendor", token, dat);
+      await onProductFetch();
+    } catch {}
+  };
+
+  const statusCheck = (e) => {
+    // if (e?.status === 200) {
+    onClear();
+    setOpen(false);
+    // } else {
+    //   enqueueSnackbar("Something went wrong", { variant: "error" });
+    // }
+  };
+
+  const handleCloseModal = async (val = "") => {
+    if (val === "submit") {
+      if (page === "products") {
+        await onAddProducts()
+          .then(statusCheck)
+          .catch((e) => {});
+      } else if (page === "product") {
+        await onEditProduct()
+          .then(statusCheck)
+          .catch((e) => {});
+      }
+    } else {
+      onClear();
+      setOpen(false);
+    }
+  };
+
+  const renderModalItem = () => {
+    const handleChange = (e, i, itm) => {
+      let temp = [...products];
+      temp[i][itm] = e.currentTarget.value;
+
+      setProducts(temp);
+    };
+    const handleDelete = (i) => {
+      products.splice(i, 1);
+      setProducts([...products]);
+    };
+
+    return products.map((itm, index) => (
       <Box
+        key={index}
         sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          mr: 10,
+          "& .MuiTextField-root": { m: 2 },
         }}
+        validate
+        autoComplete="off"
       >
-        <Button variant="contained" color="primary" onClick={onAddVendor}>
-          Add
-        </Button>
+        {Object.keys(keys[0]).map((item, indx) => (
+          <TextField
+            key={indx}
+            required
+            label={head[indx]}
+            size="small"
+            value={itm[item] ?? ""}
+            onChange={(txt) => handleChange(txt, index, item)}
+          />
+        ))}
+        <IconButton
+          color="primary"
+          onClick={() => handleDelete(index)}
+          sx={{ position: "absolute", right: 5 }}
+        >
+          <Delete />
+        </IconButton>
+        <Divider />
       </Box>
+    ));
+  };
+
+  const handleAddRowModal = () => {
+    let temp = [...products];
+    temp.push({
+      name: "",
+      address: "",
+      phone: "",
+      email: "",
+      gstno: "",
+      pancard: "",
+    });
+    setProducts(temp);
+  };
+
+  const onDeleteProduct = async (id) => {
+    try {
+      await get("delete-vendor/" + id, token);
+      await onProductFetch();
+    } catch {}
+  };
+
+  const onChangePage = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const onChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setCurrentPage(0);
+  };
+
+  const isLoaded = data?.length > 0 && data[0]?.name === "";
+
+  return (
+    <Loader load={isLoaded}>
+      <Modal
+        open={open}
+        handleAddRow={handleAddRowModal}
+        handleClose={handleCloseModal}
+        title={page === "product" ? "Edit Vendor" : "Add Vendor"}
+        page={page}
+        renderItem={renderModalItem}
+      />
+      <TableContainer>
+        <Table
+          sx={{ minWidth: window.innerWidth }}
+          size="small"
+          aria-label="a dense table"
+        >
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>
+                <Button
+                  onClick={() => handleClickOpenModal("products")}
+                  variant="contained"
+                >
+                  Add
+                </Button>
+              </StyledTableCell>
+              {head?.map((r, i) => (
+                <StyledTableCell component="th" key={i}>
+                  {r}
+                </StyledTableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data
+              ?.slice(
+                currentPage * rowsPerPage,
+                currentPage * rowsPerPage + rowsPerPage
+              )
+              ?.map((row, i) => (
+                <StyledTableRow key={i}>
+                  <StyledTableCell component="th" scope="row" align={"right"}>
+                    <IconButton
+                      color="primary"
+                      onClick={() => handleClickOpenModal("product", i)}
+                    >
+                      <Edit />
+                    </IconButton>
+                    <IconButton
+                      color="primary"
+                      onClick={() => row?.id && onDeleteProduct(row?.id)}
+                    >
+                      <Delete />
+                    </IconButton>
+                  </StyledTableCell>
+                  {Object.keys(keys[0])?.map((r, ind) => (
+                    <StyledTableCell key={ind} align={"right"}>
+                      {row[r]}
+                    </StyledTableCell>
+                  ))}
+                </StyledTableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50]}
+        component="div"
+        count={data?.length ?? 0}
+        rowsPerPage={rowsPerPage}
+        page={currentPage}
+        onPageChange={onChangePage}
+        onRowsPerPageChange={onChangeRowsPerPage}
+      />
     </Loader>
   );
 }
